9d99d68483b9a8ff813b8c17b0489d80fa4834ff
gchinmayvarma <gchinmayvarma@gmail.com>
2022-02-07 00:26:26 +0530

    idunno

--- detection.js
+++ detection.js
@@ -1,22 +1,39 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
-let display_landmarks = true;
+// videoElement.addEventListener("loadedmetadata", function (e) {
+//     this.width = this.videoWidth,
+//     this.height = this.videoHeight;
+// }, false);
+// const canvasElement = document.getElementsByClassName('output_canvas')[0];
+// const canvasCtx = canvasElement.getContext('2d');
+let display_landmarks = false;
+let first_time_setup = true;
+let detections = [];
+let videoWH = 1 ; 
+let videoScale = 1;
+
 function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-    results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
-      if (display_landmarks) {
-        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-          { color: '#FFFFFF', lineWidth: 2 });
-        drawLandmarks(canvasCtx, landmarks, { color: '#FF0040', lineWidth: 1 });
-      }
-    }
+  if (first_time_setup) {
+    first_time_setup = false;
+    videoWH = videoElement.videoWidth / videoElement.videoHeight;
+    videoElement.width = window.innerWidth;
+    p5jscanvas.position(videoElement.offsetLeft - videoElement.offsetWidth / 2, videoElement.offsetTop - videoElement.offsetHeight / 2);
+    monapple.resizeCanvas(videoElement.width, videoElement.videoHeight*videoElement.width/videoElement.videoWidth);    // mon
   }
-  canvasCtx.restore();
+  detections = results;
+  // canvasCtx.save();
+  // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  // canvasCtx.drawImage(
+  //   results.image, 0, 0, canvasElement.width, canvasElement.height);
+  // if (results.multiHandLandmarks) {
+  //   for (const landmarks of results.multiHandLandmarks) {
+  //     if (display_landmarks) {
+  //       drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+  //         { color: '#FFFFFF', lineWidth: 2 });
+  //       drawLandmarks(canvasCtx, landmarks, { color: '#FF0040', lineWidth: 1 });
+  //     }
+  //   }
+  // }
+  // canvasCtx.restore();
 }
 const hands = new Hands({
   locateFile: (file) => {
--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
-let canvas ; 
-let sketch = function ( p) { 
+let p5jscanvas ; 
+let sketch = function (p) { 
     p.setup = function () {
-        canvas = p.createCanvas(640, 480 ) ; 
+        p5jscanvas = p.createCanvas(640, 480) ;
+        p.fill(255,0,69) ;
+        p.stroke(255,0,69) ;  
+
+        p.textSize(50); 
     }
     p.draw = function () { 
         p.clear();
-        p.text("hello", 100,100) ; 
+        p.text("hello",p.width/2 , p.height/2) ; 
+        if(display_landmarks){
+            let landmarks = detections.multilandmarks[0]; 
+            for(let i = 0 ; i < landmarks.length ; i++){
+                p.ellipse(landmarks[i].x * p.width , landmarks[i].y * p.height , 10 , 10) ; 
+            }
+        }
     }
 }
 
